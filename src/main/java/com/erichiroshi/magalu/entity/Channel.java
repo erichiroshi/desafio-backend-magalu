4ed4598650142df66c4bc49a4e92aab3b4735a2a
Eric Hiroshi <erichiroshi@hotmail.com>
2025-10-31 15:18:50 -0400

    Entidades mapeadas: Notification, Channel e Status

---
+++ src/main/java/com/erichiroshi/magalu/entity/Channel.java
@@ -0,0 +1,20 @@
+package com.erichiroshi.magalu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "tb_channel")
+public class Channel {
+
+    @Id
+    private Long channelId;
+
+    private String description;
+
+}
